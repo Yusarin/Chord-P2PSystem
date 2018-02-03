aa3c6721101b45e12c31ce5c272b0b30208a2196
Russelluo <russelluo@gmail.com>
2018-02-02 21:21:40 -0600

    overload to thread and add server socket

--- Process/src/process.java
+++ Process/src/process.java
@@ -7,24 +7,42 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.net.*;
+import java.nio.channels.AsynchronousServerSocketChannel;
 import java.util.HashMap;
 
-public class process {
-    private int PROC_NUMBER;//this process's ID
-    private InetAddress IP;//this process's ip
-    private int port;// this process's port
+public class process extends Thread {
+    private final int POOL_ID = 0;//default pool ID
+    private final int PROC_ID;//this process's ID
+    private final InetAddress IP;//this process's ip
+    private final int port;// this process's port
+    private boolean running=false;
+    private AsynchronousServerSocketChannel sock;
     private HashMap<Integer, Socket> groupMembers;//map process ID's to sockets
 
     /**
      * @param number ID assigned for this process
-     * @param s IP this process bind to
+     * @param s      IP this process bound to
+     * @param poolPort Process Pool's port
+     * @param port Port this process bound to
      */
-    public process(int number, InetAddress s) {
-        PROC_NUMBER = number;
+    public process(int number, InetAddress s, int port, int poolPort) throws IOException {
+        super();
+        PROC_ID = number;
         IP = s;
+        this.port = port;
+        sock = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(IP,port));
+        groupMembers = new HashMap<Integer, Socket>();
+        groupMembers.put(POOL_ID, new Socket(IP, poolPort));
     }
+
+    @Override
+    public void run() {
+        running = true;
+        sock.accept();
+        //TODO:implement this run method
+    }
+
     /**
      * @param dst dst process id
      * @param msg message to send
@@ -63,12 +81,12 @@ public class process {
     }
 
     /**
-     * @param id new process id
-     * @param ip new process's ip
+     * @param id   new process id
+     * @param ip   new process's ip
      * @param port new process's port
      */
-    public void addNeigh(int id, InetAddress ip,int port)throws IOException {
-        Socket s = new Socket(ip,port);
+    public void addNeigh(int id, InetAddress ip, int port) throws IOException {
+        Socket s = new Socket(ip, port);
         groupMembers.put(id, s);
     }
 
@@ -76,7 +94,7 @@ public class process {
      * print this process's ID, IP, port number
      */
     public void printProcessInfo() {
-        System.out.printf("Process number is %d\n", this.PROC_NUMBER);
+        System.out.printf("Process number is %d\n", this.PROC_ID);
         System.out.printf("IP is: %s\n", IP.toString());
         System.out.printf("Port number is: %d\n", port);
     }
