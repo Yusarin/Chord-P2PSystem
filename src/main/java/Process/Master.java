cf197b397d4be4ea74ae329b0923607767b3b73e
WenhanZ <marinebluee@hotmail.com>
2018-02-25 20:53:12 -0600

    Update Master.java

--- src/main/java/Process/Master.java
+++ src/main/java/Process/Master.java
@@ -1,12 +1,13 @@
-package Process;
+package test;
+
 
 import java.io.*;
+import java.nio.channels.ServerSocketChannel;
 import java.util.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.BlockingQueue;
-
 
 public class Master extends BlockingProcess{
     private int headercounter;
@@ -20,13 +21,11 @@ public class Master extends BlockingProcess{
     private ServerSocketChannel sock;
     private int min_delay;
     private int max_delay;
-    
     public Master(BlockingQueue q, int ID, HashMap<Integer, InetSocketAddress> map, int min_delay, int max_delay) throws IOException {
         super(q,ID,map,min_delay,max_delay);
         headercounter = 0;
         this.sequence = new LinkedList<Message>();
     }
-
 
     @Override
     public void run() {
@@ -62,7 +61,6 @@ public class Master extends BlockingProcess{
             }
         }).start();
         while (true) {
-            try {
                 //Sending messages in the queue to all other processes in FIFO order.
                 final long delay = (long) (new Random().nextDouble() * (max_delay - min_delay)) + min_delay;
                 new Timer().schedule(new TimerTask() {
@@ -80,14 +78,11 @@ public class Master extends BlockingProcess{
                         }
                     }
                 }, delay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
-    }
+    
 
-    @Override
-    private String unicast_receive(int dst, byte[] msg) throws IOException {
+    public String unicast_receive(int dst, byte[] msg) throws IOException {
         SocketChannel s = idMapSocket.get(dst);
         while (true) {
             ByteBuffer sizeBuf = ByteBuffer.allocate(4);
@@ -100,7 +95,7 @@ public class Master extends BlockingProcess{
             byte[] message = new byte[content.remaining()];
             content.get(message);
             String strmsg = new String(message);
-            Message m = new Message(ipMapId.get(s.socket().getRemoteSocketAddress()), s.socket().getRemoteSocketAddress(), strmsg, headercounter);
+            Message m = new Message(ipMapId.get(s.socket().getRemoteSocketAddress()), (InetSocketAddress)s.socket().getRemoteSocketAddress(), strmsg, headercounter);
             headercounter++;
             sequence.offer(m);
             System.out.println("Sequencer Received: " + strmsg);
@@ -132,7 +127,7 @@ public class Master extends BlockingProcess{
         }
         int msg_len = msg.length;
         System.out.println("msg length: " + msg_len);
-        s.write(ByteBuffer.allocate(4).putInt(msg_len).flip());
+        s.write((ByteBuffer) ByteBuffer.allocate(4).putInt(msg_len).flip());
         s.write(ByteBuffer.wrap(msg));
     }
 
