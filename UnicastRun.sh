99e33d88712bed9ef2a3d24b4465062eb32cb302
russelluo <russelluo@gmail.com>
2018-02-28 23:11:54 -0600

    fix configuration

--- UnicastRun.sh
+++ UnicastRun.sh
@@ -3,8 +3,8 @@ for id in $(seq 1 $1)
 do
 echo "$id"
 if [ $(uname) = "Darwin" ]; then
-    osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.UnicastDemo $id configuration\""
+    osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.UnicastDemo $id UnicastConfiguration\""
 else
-    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.UnicastDemo $id configuration"
+    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.UnicastDemo $id UnicastConfiguration"
 fi
 done
