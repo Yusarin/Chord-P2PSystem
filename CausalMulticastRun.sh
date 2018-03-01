c84d00e729a4277fedefc2ab401cf44860f41430
zhaowenhan <wenhanz3@illinois.edu>
2018-02-28 23:30:41 -0600

    Working version of totalordering.

--- CausalMulticastRun.sh
+++ CausalMulticastRun.sh
@@ -2,10 +2,27 @@
 for id in $(seq 1 $1)
 do
 echo "$id"
-if [ -n "$2" ]; then
-    echo "$2/$id"
-    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration $2/$id"
+echo $(uname)
+if [ $(uname) = "Darwin" ]; then
+    if [ -n "$2" ]; then
+        echo "$2/$id"
+        osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration $2/$id\""
+    else
+        osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration\""
+    fi
 else
-    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration"
+    if [ -n "$2" ]; then
+        echo "$2/$id"
+        gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration $2/$id"
+    else
+        gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration"
+    fi
 fi
+#if (uname)
+#if [ -n "$2" ]; then
+#    echo "$2/$id"
+#    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration $2/$id"
+#else
+#    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.CausalMulticastDemo $id configuration"
+#fi
 done
---
+++ TotalorderMulticastRun.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+for id in $(seq 1 $1)
+do
+echo "$id"
+echo $(uname)
+if [ $(uname) = "Darwin" ]; then
+    if [ -n "$2" ]; then
+        echo "$2/$id"
+        osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration $2/$id\""
+    else
+        osascript -e "tell application \"Terminal\" to do script \"cd $PWD && java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration\""
+    fi
+else
+    if [ -n "$2" ]; then
+        echo "$2/$id"
+        gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration $2/$id"
+    else
+        gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration"
+    fi
+fi
+#if (uname)
+#if [ -n "$2" ]; then
+#    echo "$2/$id"
+#    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration $2/$id"
+#else
+#    gnome-terminal --tab -x zsh -c "java -cp build/libs/CS425MP1.jar Process.TotalMulticastDemo $id totalconfiguration"
+#fi
+done
